bb9f7b8f28dec508ff5f082b564dd79cbc4a3422
Andrii Halabuda <losAngelesBoy11@gmail.com>
2017-07-12 00:06:24 +0300

    Added In-App purchase code

--- GST Calculator/AppDelegate.swift
+++ GST Calculator/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        SwiftlyRater.sharedInstance.appId = "1178333093"
-        
+        SwiftlyRater.sharedInstance.appId = "1178333093" // <- Change AppID !!!!
+        SwiftlyRater.sharedInstance.usesUntilPrompt = 1
         return true
     }
 
--- GST Calculator/SettingsVC.swift
+++ GST Calculator/SettingsVC.swift
@@ -7,14 +7,72 @@
 //
 
 import UIKit
+import StoreKit
 
-class SettingsVC: UIViewController {
+class SettingsVC: UIViewController, SKProductsRequestDelegate, SKPaymentTransactionObserver {
+    
+    var products = [SKProduct]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        requestProducts()
     }
     
+    // Request products from Apple
+    func requestProducts() {
+        let IDs : Set<String> = ["com.andriiHalabuda.GSTCalculator.ad"]
+        let productsRequest = SKProductsRequest(productIdentifiers: IDs)
+        productsRequest.delegate = self
+        productsRequest.start()
+    }
+    
+    // Handling Apple response
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        print("Products ready: \(response.products.count)")
+        print("Products not ready: \(response.invalidProductIdentifiers.count)")
+        self.products = response.products
+    }
+    
+    // Restore previous purchases
+    @IBAction func restoreBtnTapped(_ sender: Any) {
+        SKPaymentQueue.default().restoreCompletedTransactions()
+    }
+    
+    // Creating payment queue
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        
+        for transaction in transactions {
+            
+            switch transaction.transactionState {
+            case .purchased:
+                print("Purchased")
+                unlockArt(transaction.payment.productIdentifier)
+                SKPaymentQueue.default().finishTransaction(transaction)
+                break
+            case .failed:
+                print("Failed")
+                SKPaymentQueue.default().finishTransaction(transaction)
+                break
+            case .restored:
+                print("Restored")
+                unlockArt(transaction.payment.productIdentifier)
+                SKPaymentQueue.default().finishTransaction(transaction)
+                break
+            case .purchasing:
+                print("Purchasing")
+                break
+            case .deferred:
+                print("Deffered")
+                break
+            }
+        }
+    }
+    
+    // Show an art when purchased or restored
+    func unlockArt(_ productIdentifier:String) {
+        
+        
+    }
     
 }
