104429a35499785e1a701af557efc0ef9155f7d6
Andrii Halabuda <losAngelesBoy11@gmail.com>
2017-07-11 19:41:09 +0300

    Settings added

--- GST Calculator/BaseVC.swift
+++ GST Calculator/BaseVC.swift
@@ -181,21 +181,21 @@ class BaseVC: UIViewController, UITextFieldDelegate {
     
     //Keyboard frame sizing
     func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height
-            }
-        }
+//        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+//            if self.view.frame.origin.y == 0 {
+//                self.view.frame.origin.y -= keyboardSize.height
+//            }
+//        }
         keyboard.isHidden = true
         doneButton.isHidden = false
     }
     
     func keyboardWillHide(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y != 0 {
-                self.view.frame.origin.y += keyboardSize.height
-            }
-        }
+//        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+//            if self.view.frame.origin.y != 0 {
+//                self.view.frame.origin.y += keyboardSize.height
+//            }
+//        }
         keyboard.isHidden = false
         doneButton.isHidden = true
     }
